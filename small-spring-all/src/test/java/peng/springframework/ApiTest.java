c93cf597d0518ccb5972322834a8b73be8cf52f6
peng <renpeng@orientsec.com.cn>
2021-09-10 17:15:22 +0800

    抽取了构建实例对象的实例化策略；能够调用带参数的构造方法构造实例对象

--- small-spring-all/src/test/java/peng/springframework/ApiTest.java
+++ small-spring-all/src/test/java/peng/springframework/ApiTest.java
@@ -1,9 +1,14 @@
 package peng.springframework;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.NoOp;
 import org.junit.Test;
 import peng.springframework.bean.UserService;
 import peng.springframework.beans.factory.config.BeanDefinition;
 import peng.springframework.beans.factory.support.DefaultListableBeanFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class ApiTest {
 
@@ -16,13 +21,46 @@ public class ApiTest {
         BeanDefinition beanDefinition = new BeanDefinition(UserService.class);
         beanFactory.registerBeanDefinition("userService", beanDefinition);
 
-        // 3.第一次获取 bean, 这时候会创建一个userservice的实例对象
-        UserService userService = (UserService) beanFactory.getBean("userService");
+        // 3.第一次获取 bean, 这时候会创建一个userservice的实例对象;
+        //   现在可以传入构造参数了
+        UserService userService = (UserService) beanFactory.getBean("userService","peng");
         userService.queryUserInfo();
 
         // 4.第二次获取 bean from Singleton,这时候缓存的就是上次创建的实例对象
         UserService userService_singleton = (UserService) beanFactory.getBean("userService");
         userService_singleton.queryUserInfo();
-
     }
+
+    @Test
+    public void test_cglib(){
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(UserService.class);
+        enhancer.setCallback(new NoOp() {
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+        });
+        Object obj = enhancer.create(new Class[]{String.class}, new Object[]{"peng"});
+        System.out.println(obj);
+    }
+
+    @Test
+    public void test_constructor() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class<UserService> userServiceClass = UserService.class;
+        Constructor<UserService> declaredConstructor = userServiceClass.getDeclaredConstructor(String.class);
+        UserService userService = declaredConstructor.newInstance("peng");
+        System.out.println(userService);
+    }
+
+    @Test
+    public void test_parameterTypes() throws Exception {
+        Class<UserService> beanClass = UserService.class;
+        Constructor<?>[] declaredConstructors = beanClass.getDeclaredConstructors();
+        Constructor<?> constructor = declaredConstructors[1];
+        Constructor<UserService> declaredConstructor = beanClass.getDeclaredConstructor(constructor.getParameterTypes());
+        UserService userService = declaredConstructor.newInstance("peng");
+        System.out.println(userService);
+    }
+
 }
--- small-spring-all/src/test/java/peng/springframework/bean/UserService.java
+++ small-spring-all/src/test/java/peng/springframework/bean/UserService.java
@@ -3,8 +3,24 @@ package peng.springframework.bean;
 
 public class UserService {
 
-    public void queryUserInfo(){
-        System.out.println("查询用户信息");
+    private String name;
+
+    public UserService() {
+    }
+
+    public UserService(String name){
+        this.name = name;
+    }
+
+    public void queryUserInfo() {
+        System.out.println("查询用户信息: " + name);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("");
+        sb.append("").append(name);
+        return sb.toString();
     }
 
 }
